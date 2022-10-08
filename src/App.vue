b20dc729c0531664d87971abd6e53b1c001c49e9
daniil.f <daniil.f@remotza.tech>
2022-10-08 10:49:17 +0700

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-    </nav>
-    <router-view/>
+    <section class="body">
+      <Sidebar /> 
+      <router-view/>
+    </section>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Sidebar from "@/components/Sidebar/Sidebar"
+export default {
+  name: 'App',
+  components: {
+    Sidebar
   }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/reset.scss';
+@import '@/assets/styles/vars.scss';
+#app {
+ background-color: $main-bg;
 }
 </style>
